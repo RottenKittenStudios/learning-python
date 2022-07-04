227ffcf0ed25c269285393df326f00558433380f
Goines <goines@users.noreply.github.com>
2022-07-04 20:32:22 +0000

    Updating basic math operations.

--- basic/app.py
+++ basic/app.py
@@ -1,4 +1,21 @@
-# TODO: Update this with actual data.
+"""Sample app for basic procedural programming concepts."""
 
-msg = "Hello World"
-print(msg)
+first_number = int(input('Please enter a number: '))
+second_number = int(input('Please enter another number: '))
+
+# Sum of these numbers:
+sum_of_numbers = 0  # Replace 0 with the sum forumla.
+
+# Difference of `first_number` and `second_numbers`:
+difference_of_numbers = 0  # Replace 0 with the difference formula.
+
+# Multiple these numbers:
+product_of_numbers = 0  # Replace 0 with the product formula.
+
+# Remainder of `first_number` divided by `second_numbers`:
+remainder_of_numbers = 0  # Replace 0 with the remainder formula.
+
+print(f'The sum of {first_number} and {second_number} is: {sum_of_numbers}')
+print(f'The difference of {first_number} and {second_number} is: {difference_of_numbers}')
+print(f'The product of {first_number} and {second_number} is: {product_of_numbers}')
+print(f'The remainder of {first_number} and {second_number} is: {remainder_of_numbers}')
